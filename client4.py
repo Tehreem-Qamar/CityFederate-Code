f62e115fa25e1d486fa67946e3f87fcd91de4de4
Tehreem-Qamar <tqresearch25@gmail.com>
2024-08-21 11:05:12 +0500

    Federated Setting - Random Data

---
+++ client4.py
@@ -0,0 +1,205 @@
+import tensorflow as tf
+from tensorflow import keras
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+from imutils import paths
+import random
+import cv2
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from sklearn.utils import shuffle
+from sklearn.metrics import accuracy_score
+from tensorflow.keras.layers import Input, GlobalAveragePooling2D, Dense, Dropout, Flatten
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import confusion_matrix, classification_report
+from tensorflow.keras.optimizers import Adam
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+import flwr as fl
+# Load and compile Keras model
+from tensorflow.keras.applications import EfficientNetB0
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+  tf.config.experimental.set_memory_growth(gpu, True)
+
+BATCH_SIZE = 8
+train_data_dir = 'D:/TQ/Federated/4/train/'
+
+test_data_dir = 'D:/TQ/Federated/4/val/'
+
+print("Loading Images..")
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    validation_split=0.2  # Set the validation split percentage
+)
+test_datagen = ImageDataGenerator(rescale=1./255,
+                                  shear_range=0.2,
+                                  zoom_range=0.2,
+                                  horizontal_flip=True)
+
+
+print("Class Counts in Training Samples:")
+total_classes = sorted(os.listdir(train_data_dir))
+print(total_classes)
+train_classes = sorted(os.listdir(train_data_dir))
+test_classes = sorted(os.listdir(test_data_dir))
+
+base_model = tf.keras.applications.MobileNetV2(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+# Freeze the base model layers
+base_model.trainable = False
+
+# Add custom classification layers on top of the base model
+inputs = Input(shape=(224, 224, 3))
+x = base_model(inputs, training=False)
+x = GlobalAveragePooling2D()(x)
+x = Dropout(0.5)(x)  # Add dropout layer with a dropout rate of 0.5
+outputs = Dense(7, activation='softmax')(x)  # Assuming binary classification
+
+# Create the full model
+model = Model(inputs, outputs)
+optimizer = Adam(learning_rate=0.001)
+model.compile(optimizer=optimizer,
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+traingen = train_datagen.flow_from_directory(train_data_dir,
+                                                   target_size=(225, 225),
+                                                   class_mode='categorical',
+                                                   classes=total_classes,
+                                                   subset='training',
+                                                   batch_size=BATCH_SIZE, 
+                                                   shuffle=True,
+                                                   seed=42)
+
+validgen = train_datagen.flow_from_directory(train_data_dir,
+                                               target_size=(225, 225),
+                                               class_mode='categorical',
+                                               classes=total_classes,
+                                               subset='validation',
+                                               batch_size=BATCH_SIZE,
+                                               shuffle=True,
+                                               seed=42)
+
+testgen = test_datagen.flow_from_directory(test_data_dir,
+                                             target_size=(225, 225),
+                                             class_mode='categorical',
+                                             classes=total_classes,
+                                             batch_size=1,
+                                             shuffle=False,
+                                             seed=42)
+
+results_list = []
+
+
+# Define Flower client
+class FlwrClient(fl.client.NumPyClient):
+    def __init__(self, model, traingen, validgen, testgen):
+        self.model = model
+        self.traingen = traingen
+        self.validgen = validgen
+        self.testgen = testgen
+        
+        
+    def get_properties(self, config):
+        """Get properties of client."""
+        raise Exception("Not implemented")
+
+    def get_parameters(self, config):
+        """Get parameters of the local model."""
+        return model.get_weights()
+
+    def fit(self, parameters, config):
+        """Train parameters on the locally held training set."""
+
+        # Update local model parameters
+        self.model.set_weights(parameters)
+
+        early_stopping = EarlyStopping(monitor="val_loss", patience=10, restore_best_weights=True)
+        callbacks = [early_stopping]
+
+        print("Starting fit..")
+
+        history = model.fit(
+            traingen,
+            epochs=5,
+            steps_per_epoch=len(traingen),
+            validation_data=validgen,
+            validation_steps=len(validgen),
+            callbacks=callbacks,
+            verbose = 1
+        )
+        # Return updated model parameters and results
+        parameters_prime = self.model.get_weights()
+        num_examples_train = len(self.traingen)
+        results = {
+            "loss": history.history["loss"][0],
+            "accuracy": history.history["accuracy"][0],
+            "val_loss": history.history["val_loss"][0],
+            "val_accuracy": history.history["val_accuracy"][0],
+        }
+        print("Local Training Metrics on client 2: {}".format(results))
+
+        
+        y_true = testgen.classes
+        # Model Evaluation
+        print("Accuracy Before Aggregation")
+        test_loss, test_accuracy = self.model.evaluate(testgen, steps=len(testgen), verbose=0)
+        print(f"Test Loss: {test_loss}")
+        print(f"Test Accuracy: {test_accuracy}")
+
+
+        # Calculate predictions on the test data
+        y_pred = model.predict(testgen)
+        y_pred_labels = np.argmax(y_pred, axis=1)  # Get the predicted class labels
+        #print(y_pred_labels, y_true)
+
+        # Calculate the confusion matrix
+        cm = confusion_matrix(y_true, y_pred_labels)
+        print("Confusion Matrix Before Aggregation:")
+        print(cm)
+
+        results_list.append(results)
+        return parameters_prime, num_examples_train, results
+
+    def evaluate(self, parameters, config):
+        """Evaluate parameters on the locally held test set."""
+
+        # Update local model with global parameters
+        self.model.set_weights(parameters)
+
+        # Evaluate global model parameters on the local test data and return results
+        y_true = testgen.classes
+        # Model Evaluation
+        print("Accuracy After Aggregation")
+        test_loss, test_accuracy = self.model.evaluate(testgen, steps=len(testgen), verbose=0)
+        print(f"Test Loss: {test_loss}")
+        print(f"Test Accuracy: {test_accuracy}")
+
+        num_examples_test = len(self.testgen)
+        # Calculate predictions on the test data
+        y_pred = model.predict(testgen)
+        y_pred_labels = np.argmax(y_pred, axis=1)  # Get the predicted class labels
+        #print(y_pred_labels, y_true)
+
+        # Calculate the confusion matrix
+        cm = confusion_matrix(y_true, y_pred_labels)
+        print("Confusion Matrix After Aggregation:")
+        print(cm)
+
+        return test_loss, num_examples_test, {"accuracy": test_accuracy}
+
+
+# Start Flower client
+client = FlwrClient(model, traingen, validgen, testgen)
+fl.client.start_numpy_client(
+    server_address="localhost:8080",
+    client=client
+)
+model.save("client4_50_fedavg.h5")
---
+++ new_server_FedAVG.py
@@ -0,0 +1,108 @@
+import flwr as fl
+from typing import List, Tuple, Dict, Optional
+from flwr.server.strategy import FedAvg
+import os
+import tensorflow as tf
+from tensorflow.keras.models import save_model
+from tensorflow.keras.optimizers import Adam
+from flwr.common import Parameters
+
+# Make TensorFlow log less verbose
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# Enable memory growth for GPUs
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(e)
+
+# Define a function to save the global model
+def save_global_model(model):
+    save_path = "global_model_final_50.h5"
+    save_model(model, save_path)
+    print(f"Global model saved to {save_path}")
+
+
+def weighted_average(metrics):
+    total_examples = 0
+    federated_metrics = {k: 0 for k in metrics[0][1].keys()}
+    for num_examples, m in metrics:
+        for k, v in m.items():
+            federated_metrics[k] += num_examples * v
+        total_examples += num_examples
+    return {k: v / total_examples for k, v in federated_metrics.items()}
+
+
+# Define a custom FedAvg strategy that saves the global model after the final round
+class CustomFedAvg(FedAvg):
+    def __init__(self, model, num_rounds, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model = model
+        self.num_rounds = num_rounds
+
+    def aggregate_fit(
+        self,
+        rnd: int,
+        results: List[Tuple[fl.server.client_proxy.ClientProxy, fl.common.FitRes]],
+        failures: List[BaseException],
+    ) -> Tuple[Optional[Parameters], Dict[str, fl.common.Scalar]]:
+        # Call the superclass method to aggregate the weights
+        aggregated_weights, metrics = super().aggregate_fit(rnd, results, failures)
+
+        if aggregated_weights is not None:
+            # Extract the weights from the aggregated Parameters object
+            weights = fl.common.parameters_to_ndarrays(aggregated_weights)
+            # Convert weights to a Keras model
+            self.model.set_weights(weights)
+            # Save the model if this is the last round
+            if rnd == self.num_rounds:
+                save_global_model(self.model)
+        
+        # Clear session to free up memory
+        tf.keras.backend.clear_session()
+        
+        return aggregated_weights, metrics
+
+# Initialize the model
+def initialize_model():
+    base_model = tf.keras.applications.MobileNetV2(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+    base_model.trainable = False
+
+    inputs = tf.keras.Input(shape=(224, 224, 3))
+    x = base_model(inputs, training=False)
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.Dropout(0.5)(x)
+    outputs = tf.keras.layers.Dense(7, activation='softmax')(x)
+
+    model = tf.keras.Model(inputs, outputs)
+    optimizer = Adam(learning_rate=0.001)
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    
+    return model
+
+# Set the number of rounds
+num_rounds = 50
+
+# Create the model and strategy
+model = initialize_model()
+strategy = CustomFedAvg(
+    model=model,
+    num_rounds=num_rounds,
+    min_available_clients=5,
+    min_evaluate_clients=5,
+    min_fit_clients=5,
+    fit_metrics_aggregation_fn=weighted_average,
+    evaluate_metrics_aggregation_fn=weighted_average
+)
+
+# Start Flower server for a specified number of rounds of federated learning
+history = fl.server.start_server(
+    strategy=strategy,
+    config=fl.server.ServerConfig(num_rounds=num_rounds)
+)
+
+final_round, acc = history.metrics_distributed["accuracy"][-1]
+print(f"After {final_round} rounds of training, the accuracy of the global model is {acc:.3%}")
